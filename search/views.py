65c1a57f24b6c8e6c44b2f119c167c536c136ee0
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-12-02 17:20:00 +0700

    views cleaned

--- search/views.py
+++ search/views.py
@@ -6,9 +6,6 @@ from elsapy.elsdoc import AbsDoc
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-import numpy as np
-from sklearn.cluster import KMeans
-from sklearn.discriminant_analysis import StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
