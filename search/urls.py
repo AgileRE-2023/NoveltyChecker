e571cac9f461dd73e215d1538cef0fdb2a08b27f
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-12-04 08:33:52 +0700

    fix: url issue

--- search/urls.py
+++ search/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('<str:user>/', views.search, name='search'),
+    path('', views.search, name='search'),
     path('report/', views.report, name='report'),
     path('list/', views.list, name='list'),
 ]
