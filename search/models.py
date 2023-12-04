42e8f2ef372a5bdf66cb15532419d7408b26579c
Hananda Aisya M <heyhanna574@gmail.com>
2023-12-04 09:47:07 +0700

    add search model

--- search/models.py
+++ search/models.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 
 # Create your models here.
 class Manuscript(models.Model):
-    manuscript_id = models.AutoField(primary_key=True, default=0)
+    manuscript_id = models.AutoField(primary_key=True, unique=True)
     manuscript_title = models.CharField(max_length=200, default='')
     manuscript_owner = models.ForeignKey(auth.models.User, on_delete=models.CASCADE, null=True)
     manuscript_abstract = models.TextField(default='')
@@ -11,8 +11,8 @@ class Manuscript(models.Model):
     novelty_score = models.IntegerField(default=1)
     similarity_percentage = models.DecimalField(decimal_places=2, max_digits=5, default=0.00)
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def _str_(self) -> str:
+        return super()._str_()
     
     def createManuscript(self, manuscript_title, manuscript_owner, manuscript_abstract, manuscript_api, novelty_score, similarity_percentage):
         self.manuscript_title = manuscript_title
@@ -20,8 +20,7 @@ class Manuscript(models.Model):
         self.manuscript_abstract = manuscript_abstract
         self.manuscript_api = manuscript_api
         self.novelty_score = novelty_score
-        self.similarity_percentage = similarity_percentage
-        self.save()  # Save the Manuscript object to the database
+        self.similarity_percentage = similarity_percentage # Save the Manuscript object to the database
 
     def deleteManuscript(self, manuscript_id):
         self.objects.filter(id=manuscript_id).delete()
