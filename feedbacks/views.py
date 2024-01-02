53a97d0b04dcf7a18bf4de7145c3494a328f7430
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-01-02 10:00:34 +0700

    feedback fixed

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -15,11 +15,14 @@ def feedback(request):
         feedback_content=request.POST.get('content')
         feedback_score=request.POST.get('rate')
         feedback_owner=request.user
-        print(feedback_content, feedback_score, feedback_owner, feedback_manuscript)
+        print(feedback_content, 'feedback score:', feedback_score, feedback_owner, feedback_manuscript)
 
-        f = Feedback()
-        f.createFeedback(feedback_owner, feedback_manuscript, feedback_content, feedback_score)
-        f.save()
+        if feedback_score == None:
+            return render(request, 'feedbacks/feedback.html', {'error': 'Give a rating or Proceed without feedback'})
+        else:
+            f = Feedback()
+            f.createFeedback(feedback_owner, feedback_manuscript, feedback_content, feedback_score)
+            f.save()
 
         return redirect('search')
     return render(request, 'feedbacks/feedback.html')
